2985ebcb3e49711bf44d83579b9ef48eaeeb0a60
Leonardo Tononi <easaaa@protonmail.com>
2024-11-04 19:19:06 +0100

    add vercel anal

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,9 @@
 <script>
 	import '../app.css';
+	import { dev } from '$app/environment';
 	let { children } = $props();
+	import { inject } from '@vercel/analytics';
+	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 {@render children()}
